3ec1af1f77afc7bbcc4b8c2c33115a43cf67f072
yann <yann.boisselier@gmail.com>
2018-04-01 18:09:34 +0200

    add env.config file to git

---
+++ env.config.js
@@ -0,0 +1,12 @@
+/**
+ * Nos variables, qui seront injectée dans next.config.js
+ * pour les rendre accessibles aussi bien du côté node que
+ * du côté du navigateur.
+ */
+module.exports = {
+  // le endpoint graphql de graphcms, qu'on peut trouver
+  // dans la partie "settings" de l'administration.
+  // Il s'agit ici du endpoint graphql "simple"
+  GRAPHCMS_GRAPHQL_API:
+    "https://api.graphcms.com/simple/v1/cjfgpbd8s1bj101427u4eu2b0"
+};
