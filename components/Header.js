acd5873b23a7593ead3edb1c3b2eeb9c7f3ae58a
yann <yann.boisselier@gmail.com>
2018-04-01 15:34:41 +0200

    use dotenv for config giles

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import config from "../services/config";
 
 export default class HeaderPorteurAide extends React.Component {
   render() {
--- services/graphcms.js
+++ services/graphcms.js
@@ -1,5 +1,4 @@
-import getConfig from "next/config";
 import { GraphQLClient } from "graphql-request";
-const { publicRuntimeConfig } = getConfig();
-const client = new GraphQLClient(publicRuntimeConfig.GRAPHCMS_GRAPHQL_API);
+import config from "./config";
+const client = new GraphQLClient(config.env.GRAPHCMS_GRAPHQL_API);
 export default client;
